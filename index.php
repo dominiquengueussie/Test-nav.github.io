cff80fcdfbf8ff37042186b3d4e756b94a081ec1
yemdom ngueussie dominique <ngueussievega@gmail.com>
2022-06-26 15:10:11 +0100

    mise en ligne de la nav

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <script type="text/javascript">
+        function RedirectionJavascript() {
+            document.location.href = "http://localhost/projectHealth/pages/accueil.html";
+        }
+    </script>
+</head>
+
+<!--<body style="background-image:url('./images/slider-bg.png');background-attachment:fixed;background-position:center;background-size:cover;background-repeat:no-repeat;text-align:center;margin-top:5%;" onLoad="setTimeout('RedirectionJavascript()', 2000)">
+    <div style="margin:auto;">
+       <img class="img-thumbnail" alt="Cinque Terre" width="400" height="400" style="opacity:.8" src="./images/loaders/heart-loading2.gif" alt="">
+    </div>
+</body>-->
+<body style="background-color:#60c2ae; text-align:center;margin-top:5%;" onLoad="setTimeout('RedirectionJavascript()', 3000)">
+    <div style="margin:auto;">
+       <img class="img-thumbnail" alt="Cinque Terre" width="400" height="400" style="opacity:.8" src="./images/loaders/heart-loading2.gif" alt="">
+    </div>
+</body>
+
+</html>
